5f2b7ca4af940df3796e88dce34fd378abd3e23e
Joel Poulin <joel@jota.one>
2023-03-22 13:48:49 +0100

    docs: fix types

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,4 @@ export declare function getReplacer(pattern: string | RegExp): Function
 export declare function getExtractor(pattern: RegExp): Function
 export declare function replace(str: string, ...maps: Record<string, string>[]): string
 export declare function replaceExpress(str: string, ...maps: Record<string, string>[]): string
-export declare function extractPlaceholders(str: string, ...maps: Record<string, string>[]): Array<string>
+export declare function extractPlaceholders(str: string): Array<string>
